092ebe60f135d94c48125408476982bedc49d757
Mohammad Takbiri <mo.takbiri@gmail.com>
2019-02-27 15:04:42 +0330

    small tweak (#4)

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@ if [[ -n ${TRAVIS_PULL_REQUEST} ]]
     curl -L "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64" \
       -o jq
     chmod +x jq
-    url="https://api.github.com/repos/org/repo/pulls/$TRAVIS_PULL_REQUEST?access_token=$GITHUB_TOKEN"
+    url="https://api.github.com/repos/org/repo/pulls/$TRAVIS_PULL_REQUEST?access_token=$GH_TOKEN"
     target_branch=$(
       curl "$url" | ./jq '.base.ref' | tr -d '"'
     )
+    echo $target_branch
