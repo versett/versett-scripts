52f4ef1b938d6bde7c14c8f6363db9c5dfce58f4
Mohammad Takbiri <mo.takbiri@gmail.com>
2019-02-27 17:20:23 +0330

    improvement in the script (#4)

--- bin/commands/test.js
+++ bin/commands/test.js
@@ -1,25 +1,11 @@
-const { green, red } = require("chalk");
+const { green } = require("chalk");
 const { log } = console;
-const fetch = require("node-fetch");
+const getGithubPRTargetBranch = require("../util/getGithubPRTargetBranch");
+
 module.exports = () => {
   const repoSlug = process.env.TRAVIS_REPO_SLUG;
   const PRNumber = process.env.TRAVIS_PULL_REQUEST;
   const githubToken = process.env.GH_TOKEN;
-  const url = `https://api.github.com/repos/${repoSlug}/pulls/${PRNumber}?access_token=${githubToken}`;
-  fetch(url)
-    .then(res => res.json())
-    .then(json => {
-      try {
-        const targetBranch = json.base.ref;
-        green(log(`the Target Branch is ${targetBranch}`));
-        return targetBranch;
-      } catch (err) {
-        red(log("Couldn't find the target branch"));
-        process.exit(1);
-      }
-    })
-    .catch(err => {
-      red(log(err));
-      process.exit(1);
-    });
+  const targetBranch = getGithubPRTargetBranch(repoSlug, PRNumber, githubToken);
+  green(log(`the Target Branch is ${targetBranch}`));
 };
--- bin/util/getGithubPRTargetBranch.js
+++ bin/util/getGithubPRTargetBranch.js
@@ -1,5 +1,22 @@
-// const { red } = require("chalk");
-// const { log } = console;
-// const fetch = require("node-fetch");
+const { red } = require("chalk");
+const { log } = console;
+const fetch = require("node-fetch");
 
-// module.exports = (ghToken, ghSlug, prNumber) => {};
+module.exports = (repoSlug, PRNumber, githubToken) => {
+  const url = `https://api.github.com/repos/${repoSlug}/pulls/${PRNumber}?access_token=${githubToken}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(json => {
+      try {
+        const targetBranch = json.base.ref;
+        return targetBranch;
+      } catch (err) {
+        red(log("Couldn't find the target branch"));
+        process.exit(1);
+      }
+    })
+    .catch(err => {
+      red(log(err));
+      process.exit(1);
+    });
+};
