c3c934657cdc0cb88fb15c3f9fe704cbfe37121b
Mohammad Takbiri <mo.takbiri@gmail.com>
2019-02-27 17:12:09 +0330

    write simple script to get the target branch (#4)

--- bin/commands/test.js
+++ bin/commands/test.js
@@ -1,6 +1,25 @@
-const { red } = require("chalk");
+const { green, red } = require("chalk");
 const { log } = console;
+const fetch = require("node-fetch");
 module.exports = () => {
-  log(red("print pull request number"));
-  log(red(process.env.TRAVIS_PULL_REQUEST));
+  const repoSlug = process.env.TRAVIS_REPO_SLUG;
+  const PRNumber = process.env.TRAVIS_PULL_REQUEST;
+  const githubToken = process.env.GH_TOKEN;
+  const url = `https://api.github.com/repos/${repoSlug}/pulls/${PRNumber}?access_token=${githubToken}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(json => {
+      try {
+        const targetBranch = json.base.ref;
+        green(log(`the Target Branch is ${targetBranch}`));
+        return targetBranch;
+      } catch (err) {
+        red(log("Couldn't find the target branch"));
+        process.exit(1);
+      }
+    })
+    .catch(err => {
+      red(log(err));
+      process.exit(1);
+    });
 };
