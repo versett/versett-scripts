645197f5794a50dd2613b6a75d52cd58dd5d6a48
Mohammad Takbiri <mo.takbiri@gmail.com>
2019-02-27 14:55:01 +0330

    get target branch by github api (#4)

--- test.sh
+++ test.sh
@@ -1,1 +1,10 @@
-echo salam
+if [[ -n ${TRAVIS_PULL_REQUEST} ]]
+    then
+    curl -L "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64" \
+      -o jq
+    chmod +x jq
+    url="https://api.github.com/repos/org/repo/pulls/$CIRCLE_PR_NUMBER?access_token=$GITHUB_TOKEN"
+    target_branch=$(
+      curl "$url" | ./jq '.base.ref' | tr -d '"'
+    )
+    echo $target_branch
