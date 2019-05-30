ef480b13adda5efd1f71472ea5e2c57edc1e693f
Tamanon Virulhakieat <tam.virulhakieat@gmail.com>
2019-05-30 15:44:36 -0400

    fix(commit validation): update to be more adaptive on the commit (#299)

--- bin/commands/test.js
+++ bin/commands/test.js
@@ -1,57 +1,55 @@
 const { execSync } = require("child_process");
-const { red, green, yellow } = require("chalk");
+const { red, green } = require("chalk");
 const { log } = console;
 const getFormattedCommitsCount = require("../util/getFormattedCommitsCount");
 
+const execGitCommand = cmd => {
+  return execSync(cmd)
+    .toString("utf8")
+    .match(/[^\r\n]+/g);
+};
+
+const error = message => log(red(`${message}`));
+const success = message => log(green(`${message}`));
+
+const validatePRCommits = () => {
+  if (process.env.PR_URL) {
+    // Get first and latest commits hash on current branch
+    const revHashs = execGitCommand(
+      "git rev-list --simplify-by-decoration --no-merges HEAD -2"
+    );
+
+    // eslint-disable-next-line no-magic-numbers
+    if (revHashs.length !== 2) {
+      error(`no revision found in this pull request ${process.env.PR_URL}`);
+      return "error";
+    }
+
+    // Get commit messages, split them into an array, and remove the SHA part from them
+    const commitMessages = execGitCommand(
+      `git log ${revHashs[1]}..${revHashs[0]} --pretty=format:"%s" --no-merges`
+    );
+
+    if (commitMessages.length === 0) {
+      error(`no commit found in this pull request ${process.env.PR_URL}`);
+      return "error";
+    }
+
+    const formattedCommitsCount = getFormattedCommitsCount(commitMessages);
+
+    if (formattedCommitsCount) {
+      success(
+        `This branch contains ${formattedCommitsCount} commit messages that follow the template.`
+      );
+    } else {
+      error(
+        "This branch doesn't have a commit message that follows the template."
+      );
+      return "error";
+    }
+  }
+};
+
 module.exports = () => {
-  const execGitCommand = cmd => {
-    return execSync(cmd)
-      .toString("utf8")
-      .match(/[^\r\n]+/g);
-  };
-
-  const error = message => log(red(`ERROR: ${message}`));
-  const warning = message => log(yellow(`WARNING: ${message}`));
-  const success = message => log(green(`${message}`));
-
-  if (!process.env.PR_URL) {
-    warning("No link pull reuqest, ignoring the commit message validation");
-    return;
-  }
-
-  // Get first and latest commits hash on current branch
-  const hashes = execGitCommand(
-    "git rev-list --simplify-by-decoration --no-merges HEAD -2"
-  );
-
-  // eslint-disable-next-line no-magic-numbers
-  if (hashes.length !== 2) {
-    log(red("no revision found via 'git rev-list'"));
-    return "error";
-  }
-
-  // Get commit messages, split them into an array, and remove the SHA part from them
-  const commitMessages = execGitCommand(
-    `git log ${hashes[1]}..${hashes[0]} --pretty=format:"%s" --no-merges`
-  );
-
-  log(commitMessages);
-
-  if (commitMessages.length === 0) {
-    error("no commit found via 'git cherry -v'");
-    return "error";
-  }
-
-  const formattedCommitsCount = getFormattedCommitsCount(commitMessages);
-
-  if (formattedCommitsCount) {
-    success(
-      `This branch contains ${formattedCommitsCount} commit messages that follow the template.`
-    );
-  } else {
-    error(
-      "This branch doesn't have a commit message that follows the template."
-    );
-    return "error";
-  }
+  return validatePRCommits();
 };
