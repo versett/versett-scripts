ac11c6188f8c1a2816bd02ebb1e9b5c87158b017
Tamanon Virulhakieat <tam.virulhakieat@gmail.com>
2019-05-30 15:05:11 -0400

    update the test scripts (#299)
    clean up (#299)
    
    --amend (#299)

--- bin/commands/test.js
+++ bin/commands/test.js
@@ -1,49 +1,54 @@
 const { execSync } = require("child_process");
-const { red, green } = require("chalk");
+const { red, green, yellow } = require("chalk");
 const { log } = console;
 const getFormattedCommitsCount = require("../util/getFormattedCommitsCount");
 
 module.exports = () => {
-  /**
-   * Get first and latest commits hash on current branch
-   */
-  const hashes = execSync(
-    "git rev-list --simplify-by-decoration --no-merges HEAD -2"
-  )
-    .toString("utf8")
-    .match(/[^\r\n]+/g);
+  const execGitCommand = cmd => {
+    return execSync(cmd)
+      .toString("utf8")
+      .match(/[^\r\n]+/g);
+  };
 
-  if (hashes.length != 2) {
+  const error = message => log(red(`WARNING:: ${message}`));
+  const warning = message => log(yellow(`WARNING:: ${message}`));
+  const success = message => log(green(`WARNING:: ${message}`));
+
+  if (!process.env.PR_URL) {
+    warning("No link pull reuqest, ignoring the commit message validation");
+    return;
+  }
+
+  // Get first and latest commits hash on current branch
+  const hashes = execGitCommand(
+    "git rev-list --simplify-by-decoration --no-merges HEAD -2"
+  );
+
+  // eslint-disable-next-line no-magic-numbers
+  if (hashes.length !== 2) {
     log(red("no revision found via 'git rev-list'"));
     return "error";
   }
 
-  /**
-   * Get commit messages, split them into an array, and remove the SHA part from them
-   */
-  const branchCommits = execSync(`git cherry -v ${hashes[1]} ${hashes[0]}`)
-    .toString("utf8")
-    .match(/[^\r\n]+/g)
-    .map(commitMsgWithSHA => commitMsgWithSHA.substring(43)); // eslint-disable-line no-magic-numbers
+  // Get commit messages, split them into an array, and remove the SHA part from them
+  const branchCommits = execGitCommand(
+    `git log ${hashes[1]}..${hashes[0]} --pretty=format:"%s" --no-merges`
+  );
 
-  if (branchCommits.length == 0) {
-    log(red("no commit found via 'git cherry -v'"));
+  if (branchCommits.length === 0) {
+    error("no commit found via 'git cherry -v'");
     return "error";
   }
 
   const formattedCommitsCount = getFormattedCommitsCount(branchCommits);
 
   if (formattedCommitsCount) {
-    log(
-      green(
-        `This branch contains ${formattedCommitsCount} commit messages that follow the template.`
-      )
+    success(
+      `This branch contains ${formattedCommitsCount} commit messages that follow the template.`
     );
   } else {
-    log(
-      red(
-        "This branch doesn't have a commit message that follows the template."
-      )
+    error(
+      "This branch doesn't have a commit message that follows the template."
     );
     return "error";
   }
